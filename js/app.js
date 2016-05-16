1bf9814b9846829a288a5dbeee52606ff2c2aeaa
Arturo Perez <arturoperez.88@live.com>
2016-05-16 12:14:00 -0700

    submit for review
    need help with buttons

---
+++ js/app.js
@@ -0,0 +1,119 @@
+//created all varibles needed with dtached elments using JQuery
+//varibles needed for the overlay, images and caption
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img id="lbImage">');
+var $caption = $('<p></p>');
+
+
+// Varibles Needed for the arrows and to filter thru the images
+var $btnPrev = $('<button id="btnPrev"> < </button>');
+var $btnNext = $('<button id="btnNext"> > </button>');
+var $btnExit = $('<button id="btnExit"> X </button>');
+var index = 0;
+
+// appended overlay to the body, and and detached elaments to overaly
+$("body").append($overlay)
+$($overlay).append($image)
+$($overlay).append($caption)
+$($overlay).append($btnPrev)
+$($overlay).append($btnNext)
+$($overlay).append($btnExit)
+
+//1. capture the click event on a link to an image 
+
+$("#imageGallery a").click(function(event) {
+
+	//This prevents the link from going to its defult location.
+	event.preventDefault();
+
+  //This allows the overlay to show when you click on a photo 
+	$overlay.fadeIn(1000);
+
+	//This gets the href value from the images that get clicked on. and i added a new varible to it, to distnguish it, in the next step.
+	var $imageLocation = $(this).attr("href");
+
+// this updates overlay with the image that was clicked. It adds the href from the <a> to the detached <img>  src atribute.
+	$image.attr("src", $imageLocation);
+
+// This gets childes data-title
+	var captionText = $(this).children("img").attr("data-title");
+  // This adds the text from the data-title 
+	$caption.text(captionText);
+
+  $currentImg = $(this).children("img");
+
+});
+
+
+
+
+
+
+///////////////////////////////// THIS IS THE PART I CANT FIGURE OUT ///////////////////////////////////////////////////
+///////////////////////////////// NEED HELP WITH GETTING MY PREV AND NEXT BUTTONS TO WORK//////////////////////////////
+var items = $("li").children();
+var itemAmount = items.length;
+console.log(itemAmount);
+
+$btnNext.click(function(){
+
+});
+
+
+$btnNext.click(function(){
+
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+//When X is clicked
+$("#btnExit").click(function() {
+    //Hide the overlay
+    $overlay.slideUp(500);
+});
+
+
+//Hide overaly when clicked on
+$overlay.click(function(){
+	$overlay.fadeOut(1000);
+});
+
+
+////////////////////SEARCH BOX/////////////////////
+
+//this just creats an varible for the input element
+var $search = $('input');
+
+// This gets the value of what the users type into the input box and the .tolowerCaase converts evrything to a lowercase.// 
+$search.keyup(function(){
+  var userInput = $(this).val().toLowerCase();
+
+    //This adds the same function to each <img> elemnt
+  $('#imageGallery img').each(function(){
+    var altText = $(this).attr('alt').toLowerCase();
+ 
+
+    if(altText.search(userInput) > -1){
+      $(this).parent().parent().fadeIn(500);  
+    } else {                                     
+      $(this).parent().parent().fadeOut(500); 
+    }
+  });
+});
+
+  ///////////NOTS FOR SERCH BOX CODE////////////
+  /* .parent() allows you to taget the parent of an elemant, 
+  In this case the parent of the image witch is the <a> but,
+   since the img and <a> are both childes of the list item, 
+   the fadeIn and fadeOut methds dont work very well. By adding .parent() . parent() twice, 
+   it allows it to go back twice and target the <li> witch makes things fade out properly */ 
+
